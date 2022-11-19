f8559153d2455271bd485a87b03240d1b0d2a569
Sunny Raj Rathod <42240802+sunny-raj-rathod@users.noreply.github.com>
2022-11-19 14:33:51 +0000

    Bootstraping supplier services
    Bootstraping two separate services for crazyair and toughjet.

---
+++ supplier-services/toughjet/src/main/java/org/deblock/exercise/ToughJetFlightApplication.java
@@ -0,0 +1,13 @@
+package org.deblock.exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToughJetFlightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToughJetFlightApplication.class, args);
+	}
+
+}
---
+++ supplier-services/toughjet/src/test/java/org/deblock/exercise/ToughJetFlightApplicationTests.java
@@ -0,0 +1,13 @@
+package org.deblock.exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToughJetFlightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
